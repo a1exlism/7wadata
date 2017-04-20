59db8bbaab0d062c9c7666cca0e4e64767101690
Quinn Sheng <shengjitian@gmail.com>
2017-04-20 16:53:46 +0800

    depart codes from excel into projcets

--- user/models/Excels.php
+++ user/models/Excels.php
@@ -10,41 +10,7 @@ class Excels extends CI_Model
 		$this->load->dbforge();
 	}
 	
-	public function proj_nums($user_id)
-	{
-		$this->db->select('proj_id');
-		$this->db->distinct();
-		$this->db->where('user_id', $user_id);
-		$query = $this->db->get('projs');
-		return $query->num_rows();
-	}
-	
-	public function get_excel_nums($user_id, $proj_id)
-	{
-		//  获取用户特定proj存在的excel numbers
-		//  proj中第一个excel是alter操作, 之后为insert操作
-		$this->db->where(array(
-			'user_id' => $user_id,
-			'proj_id' => $proj_id,
-			'type !=' => 0
-		));
-		return $this->db->get('projs')->num_rows();
-	}
-	
-	public function projs_complete($excel_id, $type)
-	{
-		//  针对于projs第一行
-		$this->db->where('excel_id', $excel_id);
-		$this->db->update('projs', array(
-			'type' => $type
-		));
-	}
-	
-	public function projs_new($data)
-	{
-		//  $data => array
-		$this->db->insert('projs', $data);
-	}
+
 	
 	public function create_table($table_name, $tables)
 	{
